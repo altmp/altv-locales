589e528a05b7bdb79a4ddcfc502d5f7c5744a467
Artem Dzhemesiuk <zziger07@gmail.com>
2023-02-06 23:24:11 +0100

    Add pluralization and RTL support

--- README.md
+++ README.md
@@ -9,3 +9,15 @@ All credits go to the authors.
 
 # Adding your lang
 When you wanna any language to appear in alt:V Multiplayer copy en.json to [locale_code].json and replace all string entries. Then find your flag in the repo above and copy it under flags/ directory 
+
+# Plural keys
+
+For plural keys use keys in `KEY_NAME:PLURAL_TYPE` format. List of plural types and rules can be found [here](https://unicode-org.github.io/cldr-staging/charts/37/supplemental/language_plural_rules.html) (beware that plural type should be specified in the uppercase).
+Lookup order for plural keys is:
+- `KEY_NAME:EXPECTED_PLURAL_TYPE`
+- `KEY_NAME`
+- `KEY_NAME:OTHER`
+
+## Currently supported plural keys:
+- `PLAYERS_ONLINE`
+- `SERVERS_ONLINE`
--- test/test.js
+++ test/test.js
@@ -1,68 +1,75 @@
 const fs = require('fs');
 const path = require('path');
-const assert = require('assert');
+
 const langFolder = "langs";
 const args = process.argv.slice(2);
 let files = [];
-if(args.length == 0) {
-  for(const filename of fs.readdirSync(langFolder)) {
-    const filePath = path.join(langFolder, filename);
-    if(fs.lstatSync(filePath).isDirectory() || filename === "en.json") continue;
-    const ext = filename.split('.').slice(1).join('.');
-    if(ext !== "json") continue;
-    files.push(filename);
-  }
-} else {
-  for(const filePath of args) {
-    if(!filePath.startsWith(langFolder)) continue;
-    files.push(path.basename(filePath));
-  }
-}
-let file = fs.readFileSync(path.join(langFolder, "en.json"), "utf8");
-let tmpl;
-try {
-  tmpl = Object.entries(JSON.parse(file).strings);
-} catch(err) {
-  console.log(`\x1b[31mTemplate file is corrupt, aborting\x1b[0m`);
-  process.exitCode = 1;
-  return;
-}
-let failed = false;
-for(const filename of files) {
-  const dirPath = path.join(langFolder, filename);
-  if(fs.lstatSync(dirPath).isDirectory()) continue;
-  console.log(`Testing locale file ${filename} . . .`);
-  file = fs.readFileSync(dirPath, "utf8");
-  let arr;
-  const diff = [];
-  try {
-    arr = Object.entries(JSON.parse(file).strings);
-  } catch(err) {
-    failed = true;
-    console.log(`  \x1b[31m${err.message}\x1b[0m`);
-    break;
-  }
-  let different = false;
-  for(let i = 0; i < tmpl.length; i++) {
-    const oldKey = tmpl[i][0];
-    const oldVal = tmpl[i][1];
-    const newKey = i < arr.length ? arr[i][0] : null;
-    const newVal = i < arr.length ? arr[i][1] : null;
-    if(oldKey === newKey) continue;
-    failed = different = true;
-    diff.push({actual: [newKey, newVal], expected: [oldKey, oldVal]});
-  }
-  if(different) {
-    for(const elem of diff) {
-      const actual = elem.actual;
-      const expected = elem.expected;
-      console.log(`  \x1b[31m-\t"${actual[0]}": "${actual[1]}"\x1b[0m`);
-      console.log(`  \x1b[32m+\t"${expected[0]}": "${actual[1]}"\x1b[0m`);
+
+if (args.length === 0) {
+    for (const filename of fs.readdirSync(langFolder)) {
+        const filePath = path.join(langFolder, filename);
+        if (fs.lstatSync(filePath).isDirectory() || filename === "en.json") continue;
+        if (!filename.match(/\.json$/)) continue;
+        files.push(filename);
     }
-    console.log(`  \x1b[32m+ expected\x1b[0m \x1b[31m- actual\x1b[0m`);
-    console.log(`  \x1b[31mError: File contents differ.\x1b[0m`);
-  } else {
-    console.log(`  \x1b[32mAll checks passed\x1b[0m`);
-  }
+} else {
+    for (const filePath of args) {
+        if (!filePath.startsWith(langFolder)) continue;
+        files.push(path.basename(filePath));
+    }
 }
-if(failed) process.exitCode = 1;
+
+function transformKeys(keys) {
+    keys = keys.map(e => e.endsWith(':OTHER') ? e.slice(0, -6) : e).filter(e => !e.includes(':'));
+    return [...new Set(keys)];
+}
+
+let baseKeys;
+try {
+    baseKeys = transformKeys(Object.keys(JSON.parse(fs.readFileSync(path.join(langFolder, "en.json"), "utf8")).strings));
+} catch (err) {
+    console.log(`\x1b[31mTemplate file is corrupt, aborting\x1b[0m`);
+    process.exit(1);
+    return;
+}
+
+let failed = false;
+for (const filename of files) {
+    const dirPath = path.join(langFolder, filename);
+    if (fs.lstatSync(dirPath).isDirectory()) continue;
+    console.log(`Testing locale file ${filename}...`);
+
+    const diff = [];
+    let keys;
+    try {
+        keys = transformKeys(Object.keys(JSON.parse(fs.readFileSync(dirPath, "utf8")).strings));
+    } catch (err) {
+        failed = true;
+        console.log(`  \x1b[31m${err.message}\x1b[0m`);
+        break;
+    }
+
+    let different = false;
+    for (let i = 0; i < baseKeys.length; i++) {
+        const oldKey = baseKeys[i];
+        const newKey = i < keys.length ? keys[i] : null;
+        if (oldKey === newKey) continue;
+        failed = different = true;
+        diff.push({ actual: newKey, expected: oldKey });
+    }
+
+    if (different) {
+        for (const elem of diff) {
+            const actual = elem.actual;
+            const expected = elem.expected;
+            console.log(`  \x1b[31m-\t"${actual}"\x1b[0m`);
+            console.log(`  \x1b[32m+\t"${expected}"\x1b[0m`);
+        }
+        console.log(`  \x1b[32m+ expected\x1b[0m \x1b[31m- actual\x1b[0m`);
+        console.log(`  \x1b[31mError: File contents differ.\x1b[0m`);
+    } else {
+        console.log(`  \x1b[32mAll checks passed\x1b[0m`);
+    }
+}
+
+if (failed) process.exit(1);
